c96591738bba4a8a14996ac98c86763554b9b55e
lewis-kori <lewiskosh@gmail.com>
2020-11-15 07:54:31 +0300

    user suspension logic added

--- core/template_urls.py
+++ core/template_urls.py
@@ -1,11 +1,14 @@
-from django.urls import path, include
+from django.urls import include, path
 
-from .views import IndexTemplateView
+from .views import (IndexTemplateView, UserConfirmSuspend,
+                    toggle_user_suspension)
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path('', IndexTemplateView.as_view(), name='index'),
+    path('users/confirm-suspend/<int:pk>/',UserConfirmSuspend.as_view(),name='confirm_suspend'),
+    path('users/suspend/<int:pk>/',toggle_user_suspension,name='suspend'),
     path('members/', include('core.templateurls.members')),
     path('trainers/', include('core.templateurls.trainers')),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
+from django.contrib import messages
 from django.contrib.auth import get_user_model
-from django.views.generic import TemplateView
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView, DetailView
 
-from .mixins import AdminDashBoardMixin
+from .mixins import AdminDashBoardMixin, check_rights
 from .models import GymClass, PersonalTraining
 
 User = get_user_model()
@@ -21,3 +25,27 @@ class IndexTemplateView(AdminDashBoardMixin, TemplateView):
         context['personal_training_sessions'] = PersonalTraining.objects.filter(is_accepted=True).count()
         context['gym_classes'] = GymClass.objects.all().count()
         return context
+
+# accepted an incoming order
+@login_required
+@user_passes_test(check_rights)
+def toggle_user_suspension(request, pk):
+    user = User.objects.get(pk=pk)
+
+    user.is_active = not user.is_active
+
+    # put email send here
+    
+    user.save()
+    messages.success(request, "User has been deactivated")
+    if user.role == 'Member':
+        return HttpResponseRedirect(reverse("dashboard:members_dashboard:member_details",
+                            kwargs={"pk": user.id}))
+    elif user.role == 'Trainer':
+        return HttpResponseRedirect(reverse("dashboard:trainers_dashboard:trainer_details",
+                            kwargs={"pk": user.id}))
+
+class UserConfirmSuspend(AdminDashBoardMixin, DetailView):
+    model = User
+    template_name = 'account/confirm-suspend.html'
+    context_object_name = 'user'
